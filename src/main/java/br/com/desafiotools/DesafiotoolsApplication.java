5a61ac897f6f5cfa438e5c98b468d709303dc4f1
KaikeTinoco <kaiketinocon@outlook.com>
2024-04-13 23:36:35 -0300

    primeiro commit

---
+++ src/main/java/br/com/desafiotools/DesafiotoolsApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafiotools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafiotoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafiotoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafiotools/DesafiotoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafiotools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiotoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
