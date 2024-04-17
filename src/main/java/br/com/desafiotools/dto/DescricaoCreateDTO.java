acfc85ce311a7e6d07984ef0b153220ffbaf6c95
KaikeTinoco <kaiketinocon@outlook.com>
2024-04-17 19:51:49 -0300

    Oitavo Commit: só adicionei isso aqui no DescricaoCreateDTO @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)

--- src/main/java/br/com/desafiotools/dto/DescricaoCreateDTO.java
+++ src/main/java/br/com/desafiotools/dto/DescricaoCreateDTO.java
@@ -1,14 +1,21 @@
 package br.com.desafiotools.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 
 @Setter
 @Getter
 public class DescricaoCreateDTO {
     private BigDecimal valor;
-    private String dataHora;
+
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
+    private LocalDateTime dataHora;
+
     private String estabelecimento;
 }
